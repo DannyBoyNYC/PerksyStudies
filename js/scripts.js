0ff26f19380c11606040fe49c109f84e4ea7f5cb
DannyBoyNYC <daniel.deverell@gmail.com>
2020-05-17 15:05:18 -0400

    initial commit

---
+++ js/scripts.js
@@ -0,0 +1,26 @@
+function addNavClass(e) {
+  // console.log("on");
+  header.classList.add("header-switch");
+}
+
+function removeNavClass() {
+  // console.log("off");
+  header.classList.remove("header-switch");
+}
+
+const header = document.querySelector("header");
+header.addEventListener("mouseover", addNavClass);
+header.addEventListener("mouseout", removeNavClass);
+
+function showSubNav(e) {
+  subnav.style.display = "block";
+}
+
+function hideSubNav(e) {
+  subnav.style.display = "none";
+}
+
+const captivate = document.querySelector(".link-captivate");
+const subnav = document.querySelector(".subnav");
+captivate.addEventListener("mouseover", showSubNav);
+captivate.addEventListener("mouseout", hideSubNav);
