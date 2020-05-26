fd05db9ef109e94d17f7d86ac4fb658d3068b39f
DannyBoyNYC <daniel.deverell@gmail.com>
2020-05-26 14:30:42 -0400

    404 builds

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,10 +1,10 @@
-import React from "react"
-import { Layout } from "../components/Layout"
+import React from "react";
+import { Layout } from "../components/Layout";
 
-export const fourOFour = () => {
+export default () => {
   return (
     <Layout>
       <p>Page not found</p>
     </Layout>
-  )
-}
+  );
+};
